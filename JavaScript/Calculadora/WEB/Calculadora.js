56295ea6054a9c831bba234ae32d0e3efa5e3296
sebass <sebaslomba23@gmail.com>
2024-12-04 10:49:08 -0300

    Trabajos de Javascript

--- JavaScript/Calculadora/WEB/Calculadora.js
+++ JavaScript/Calculadora/WEB/Calculadora.js
@@ -1,6 +1,20 @@
 let pantalla = document.getElementById("pantalla")
 let operador = null;
 let num1 = null;
+let num2 = null;
+let esperandoSegundoNumero = false;
+
+function escribirNumero(numeroo)
+{
+    if(esperandoSegundoNumero)
+    {
+        pantalla.value = numeroo;
+        esperandoSegundoNumero = false;
+    }else
+    {
+        pantalla.value += numeroo;
+    }
+}
 
 function seguirAgregando(numero)
 {
@@ -11,43 +25,55 @@ function operar(op)
 {
     if(pantalla.value !== "")
     {
-        num1 = parseFloat(pantalla.value);
-        operador = op;
-        pantalla.value = "";
+        if(num1 === null)
+        {
+            num1 = parseFloat(pantalla.value);
+            operador = op;
+            esperandoSegundoNumero = true;
+            pantalla.value += (`${op}`);
+        }else
+        {
+            num2 = parseFloat(pantalla.value);
+            calcular();
+            operador = op;
+            pantalla.value += ` ${op} `;
+            esperandoSegundoNumero = true;
+        }
     }
 }
 function calcular()
 {
-    if((operador !== null) && (num1 !== null) && (pantalla.value !== ""))
+    if((num1 !== null) && (operador !== null))
     {
-        const num2 = parseFloat(pantalla.value);
-        let resultado;
+        let partes = pantalla.value.split(`${operador}`)
+        num2 = parseFloat(partes[1]);
 
         switch (operador)
         {
             case "+":
-                resultado = num1+num2;
+                num1 = num1+num2;
                 break;
             case "-":
-                resultado= num1-num2;
+                num1= num1-num2;
                 break;
-            case "*":
-                resultado= num1*num2;
+            case "×":
+                num1= num1*num2;
                 break;
-            case "/":
+            case "÷":
                 if (num2 === 0)
                 {
                     pantalla.value= "Math Error"
                     return;
                 }
-                resultado= num1/    num2;
+                num1= (num1/num2);
                 break;
             default:
-                resultado = "Error: operador no valido";
+                num1 = "Error: operador no valido";
         }
-        pantalla.value = resultado;
+        pantalla.value = num1;
         operador = null;
-        num1 = null;
+        num2 = null;
+        esperandoSegundoNumero = false;
     }
 }
 function limpiarPantalla()
