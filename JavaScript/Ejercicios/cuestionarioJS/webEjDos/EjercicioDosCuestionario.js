e2b4a6d713ac14f2347f2e95bb053dda1d91c350
sebass <sebaslomba23@gmail.com>
2024-12-04 15:29:50 -0300

    agregados

---
+++ JavaScript/Ejercicios/cuestionarioJS/webEjDos/EjercicioDosCuestionario.js
@@ -0,0 +1,36 @@
+function raizNegativa(a,b,c)
+{
+
+    const d = b**2 - (4*a*c);
+    if(d<0)
+    {
+        return "No existen soluciones reales ";
+    }
+    //RAIZ NEGATIVA
+    const raizN = ((-b - Math.sqrt(d))) / (2*a);
+    return raizN;
+}
+    document.getElementById("ecuacion").addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    const a = parseFloat(document.getElementById("a").value);
+    const b = parseFloat(document.getElementById("b").value);
+    const c = parseFloat(document.getElementById("c").value);
+
+    if(a === 0)
+    {
+        document.getElementById("resultado").textContent = "el denominador no puede ser igual a 0";
+        return;
+
+    }
+
+    const resultado = raizNegativa(a,b,c);
+    document.getElementById("resultado").textContent = `el resultado de la version negativa de la ec es igual a  ${resultado}`
+});
+//ejemplo de uso
+
+/* const a= 5;
+const b=30;
+const c = 2;
+const resultado = raizNegativa(a,b,c);
+
+console.log(resultado);  */
